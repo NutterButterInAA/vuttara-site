759c33a1a6652e6ddad79dbc7f74b6deb45eedce
Ward Reynolds <289339067+NutterButterInAA@users.noreply.github.com>
2026-07-30 14:51:09 -0600

    Publish Vuttara 3.2.28 website

--- public/site.js
+++ public/site.js
@@ -1,6 +1,6 @@
 const REPO = "NutterButterInAA/Vuttara";
 const RELEASES_URL = `https://github.com/${REPO}/releases`;
-const FALLBACK_VERSION = "3.2.27";
+const FALLBACK_VERSION = "3.2.28";
 
 function versionNumber(tag = "") {
   return String(tag).replace(/^v/i, "").trim();
